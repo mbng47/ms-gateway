682aa79b141f09e58d71c8cd5cff8892d43dd380
Martin <martinbungner@hotmail.com>
2023-11-24 13:33:03 +0100

    Update post.ts

--- src/app/component/use-cases/post.ts
+++ src/app/component/use-cases/post.ts
@@ -5,40 +5,44 @@ export default function createPost({
   logger,
 }) {
   return Object.freeze({ post });
-  
+
   async function post({
     params,
     cacheConfig,
     JWTSecret,
     path,
     errorMsgs
-  }){
+  }) {
     try {
 
       if (path.includes('registration')) {
         const destinationPath = 'http://localhost:3011/api/v1/user'
 
-        const results = await makeFetch({ params, path: destinationPath, method: 'post'})
+        const results = await makeFetch({ params, path: destinationPath, method: 'post' })
 
         return results.json();
       }
 
       if (path.includes('auth')) {
+        try{
         const authPath = 'http://localhost:3011/api/v1/user/auth'
-        const authResults = await makeFetch({ params, path: authPath, method: 'post'})
+        const authResults = await makeFetch({ params, path: authPath, method: 'post' })
         // console.log(params);
         const results = await authResults.json()
         console.log(results)
-      
+        if (results.err > 0) {
+          throw results.data
+        }
+
         const token = results.data[0].email;
 
         const tokenPath = 'http://localhost:3010/api/v1/token'
-        const tokenResult = await makeFetch({ params: { token }, path: tokenPath, method: 'post'})
-        
+        const tokenResult = await makeFetch({ params: { token }, path: tokenPath, method: 'post' })
+
         const tokenRes = await tokenResult.json()
 
         setCache({ data: results.data[0], cacheKey: tokenRes.data, cacheConfig })
-        
+
         return ({
           error: 0,
           data: {
@@ -47,9 +51,14 @@ export default function createPost({
           }
         })
       }
-    } catch(err) { 
-      throw err
+      catch (err) {
+        throw new Error(err)
+      }
     }
     
+    } catch (err) {
+      throw err
+    }
+
   }
 }
